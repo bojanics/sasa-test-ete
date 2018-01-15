71fbf44b18d3ee9a2d69a4ed2a6fd7150995470d
Sasa Bojanic <s.bojanic@togetherteam.co.th>
2018-01-15 15:15:01 +0100

    folder restruct

--- defs/header.json.js
+++
@@ -1,9 +0,0 @@
-var headerObj = {
-    "use outlook settings": true,
-    "environment": true,
-    "notifications": true,
-    "settings": true,
-    "help": true,
-    "account": true,
-    "theme settings": true
-};
