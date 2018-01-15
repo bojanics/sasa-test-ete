d10d3cf6d55dbfcc6b7b327a6d32fe85535aea79
Sasa Bojanic <s.bojanic@togetherteam.co.th>
2018-01-15 13:29:40 +0100

    test ADAL init changed 6

--- scripts/auth.js
+++ scripts/auth.js
@@ -34,7 +34,7 @@ Logging = {
     var shouldUseADAL = isIfrm || isSignedInUser() || isCallback || qs['online']=='true';
     console.log('should use ADAL: '+shouldUseADAL);
     if (shouldUseADAL) {
-         if (ADAL!=null) {
+         if (ADAL==null) {
             ADAL = new AuthenticationContext({
                 instance: 'https://login.microsoftonline.com/',
                 tenant: 'b4a7cf6c-8876-456a-b97f-1e2bbeb7579a', //COMMON OR YOUR TENANT ID
