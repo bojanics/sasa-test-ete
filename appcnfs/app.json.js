3f0e3b178eb18bfc7e17cccfa4c0af83c3118a27
Sasa Bojanic <s.bojanic@togetherteam.co.th>
2018-01-29 16:21:24 +0100

    fixed deliberate mistake

--- appcnfs/app.json.js
+++ appcnfs/app.json.js
@@ -2,5 +2,5 @@ var appObj = {
     "formdefinition": "../forms/hdrtest1/v1/form.json.js",
     "branddefinition": "../brands/brand.json.js",
     "customizationdefinition": "../cstmzs/customization.json.js",
-    "headerconfiguration": "./hdrcnfs/header.json.js"
+    "headerconfiguration": "../hdrcnfs/header.json.js"
 };
