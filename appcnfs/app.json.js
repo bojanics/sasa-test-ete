e1b8aceef039104bf103062ca2243a979b7270b4
Sasa Bojanic <s.bojanic@togetherteam.co.th>
2018-01-29 15:01:42 +0100

    added config folders

---
+++ appcnfs/app.json.js
@@ -0,0 +1,6 @@
+var appObj = {
+    "formdefinition": "../forms/hdrtest1/v1/form.json.js",
+    "branddefinition": "../brands/brand.json.js",
+    "customizationdefinition": "../cstmzs/customization.json.js",
+    "headerconfiguration": "../hdrcnfs/header.json.js"
+};
---
+++ cstmzs/customization.json.js
@@ -0,0 +1,2 @@
+var customizationObj = {
+}
