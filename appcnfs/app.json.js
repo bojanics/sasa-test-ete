1c9b5c3b62f74f1b84facffcd4dc404be87bc3db
Sasa Bojanic <s.bojanic@togetherteam.co.th>
2018-01-29 14:02:36 +0100

    changed config

--- appcnfs/app.json.js
+++ appcnfs/app.json.js
@@ -1,6 +1,6 @@
 var appObj = {
-    "formdefinition": "../forms/offer/singlecar/v1/form.json.js",
+    "formdefinition": "../forms/hdrtest1/v1/form.json.js",
     "branddefinition": "../brands/brand.json.js",
     "customizationdefinition": "../cstmzs/customization.json.js",
-    "headerconfiguration": "./hdrcnfs/header.json.js"
+    "headerconfiguration": "../hdrcnfs/header.json.js"
 };
