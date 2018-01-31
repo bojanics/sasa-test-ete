c88d2789feaecd002fa30dba6944fc82ac6e3fe7
Sasa Bojanic <s.bojanic@togetherteam.co.th>
2018-01-31 17:11:45 +0100

    test3

--- appcnfs/app.json.js
+++ appcnfs/app.json.js
@@ -2,5 +2,5 @@ var appObj = {
     "formdefinition": "../forms/hdrtest1/v1/form.json.jss",
     "branddefinition": "../brands/brand.json.jss",
     "customizationdefinition": "../cstmzs/customization.json.jss",
-    "headerconfiguration": "../hdrcnfs/header.json.jss"
+    "headerconfiguration": "../hdrcnfs/header.json.js"
 };
