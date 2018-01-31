149f161d21f7bffb114392bc69ca8d6a886d275a
Sasa Bojanic <s.bojanic@togetherteam.co.th>
2018-01-31 16:08:20 +0100

    test vlada2

--- appcnfs/app.json.js
+++ appcnfs/app.json.js
@@ -1,6 +1,6 @@
 var appObj = {
     "formdefinition": "../forms/hdrtest1/v1/form.json.js",
     "branddefinition": "../brands/brand.json.js",
-    "customizationdefinition": "../cstmzs/customization.json.jss",
+    "customizationdefinition": "../cstmzs/customization.json.js",
     "headerconfiguration": "../hdrcnfs/header.json.js"
 };
